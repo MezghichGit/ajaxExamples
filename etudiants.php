c4cb470c121f32b3e0232b0c4fba837e9036f87d
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-07-30 15:55:03 +0100

    exo 2 recherche avec Ajax dans une BD via php

---
+++ etudiants.php
@@ -0,0 +1,35 @@
+
+
+<?php
+
+// connexion via php à mysql
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// selection de la base de données
+mysqli_select_db($conn,'isika2021');
+
+// récupération des produits
+$etudiants = $conn->query("SELECT * FROM etudiant");  // query = requette
+
+// afficher les données dans un tableau
+
+echo"<table align=center border=1>";
+echo"<tr><th>ID</th><th>Nom</th><th>Email</th><th>Ville</th></tr>";
+
+foreach ($etudiants as $etudiant) {
+    
+    echo"<tr><td>".$etudiant["id"]."</td><td>".$etudiant["nom"]."</td><td>".$etudiant["email"]."</td><td>".$etudiant["ville"]."</tr>";
+}
+
+echo"</table>";
+?>

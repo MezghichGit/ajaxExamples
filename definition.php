08586e5d0cd13ace575cdae864e8f57db584e914
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-08-18 11:42:22 +0100

    correction exo ajax tableau statique

---
+++ definition.php
@@ -0,0 +1,25 @@
+<?php
+
+//echo "<h1>Ajax est une technique Javascript pour la mise à jour partielle d'une page web</h1>";
+
+echo"
+<table border=1 align=center>
+    <tr>
+        <th>id</th>
+        <th>framework</th>
+        <th>langage</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Angular</td>
+        <td>Javascript</td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>Symfony</td>
+        <td>PHP</td>
+    </tr>
+</table>
+";
+
+?>
